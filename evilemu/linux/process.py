c8da2c7090b89a0cd1c5f1291f488386447e050e
Daid <daid303@gmail.com>
2022-11-27 18:57:22 +0100

    Fix mypy issues

--- evilemu/linux/process.py
+++ evilemu/linux/process.py
@@ -6,18 +6,18 @@ from typing import Generator, Tuple
 class Process(evilemu.processbase.ProcessBase):
     @staticmethod
     def find_processes(executable_name: str) -> Generator["Process", None, None]:
-        for pid in os.listdir("/proc/"):
+        for pid_str in os.listdir("/proc/"):
             try:
-                pid = int(pid)
+                pid_int = int(pid_str)
             except ValueError:
                 continue
             try:
-                exe = os.readlink(f"/proc/{pid}/exe")
+                exe = os.readlink(f"/proc/{pid_int}/exe")
             except IOError:
                 continue
             exe = os.path.basename(exe)
             if exe == executable_name:
-                yield Process(pid)
+                yield Process(pid_int)
 
     def __init__(self, pid: int):
         self.__mem = open(f"/proc/{pid}/mem", "rb")
@@ -36,21 +36,21 @@ class Process(evilemu.processbase.ProcessBase):
 
     def _primary_memory_section(self) -> Generator[Tuple[int, int], None, None]:
         for line in open(f"/proc/{self.__pid}/maps", "rt"):
-            line = line.split(None, 5)
-            if len(line) > 5:
-                if line[1].startswith("rw"):
-                    start, end = line[0].split("-", 1)
-                    start = int(start, 16)
-                    end = int(end, 16)
+            parts = line.split(None, 5)
+            if len(parts) > 5:
+                if parts[1].startswith("rw"):
+                    start_str, end_str = parts[0].split("-", 1)
+                    start = int(start_str, 16)
+                    end = int(end_str, 16)
                     yield start, end - start
                     return
 
     def _all_memory_sections(self) -> Generator[Tuple[int, int], None, None]:
         for line in open(f"/proc/{self.__pid}/maps", "rt"):
-            line = line.split(None, 5)
-            if len(line) > 5:
-                if line[1].startswith("rw"):
-                    start, end = line[0].split("-", 1)
-                    start = int(start, 16)
-                    end = int(end, 16)
+            parts = line.split(None, 5)
+            if len(parts) > 5:
+                if parts[1].startswith("rw"):
+                    start_str, end_str = parts[0].split("-", 1)
+                    start = int(start_str, 16)
+                    end = int(end_str, 16)
                     yield start, end - start
